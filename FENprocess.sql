38b888803817eba692362c26f9d914aa7583a38b
Alex Hall <alexhall1993@hotmail.co.uk>
2025-04-26 21:45:41 +0100

    Update FENprocess.sql
    somewhat working function.  returns the first number in the string

--- FENprocess.sql
+++ FENprocess.sql
@@ -1,6 +1,12 @@
 -- create function to find the number of fre squares in a fen 
-create function FENfreeSquares(@FEN nvarchar) returns int
+create or alter function FENfreeSquares(@FEN nvarchar(80)) returns int
 begin
-return substring(@FEN,patindex('%[1-8]%', @FEN),1)
+return convert(int, substring(@FEN,patindex('%[1-8]%', @FEN),1))
 end
 
+
+go
+
+declare @FEN nvarchar(max);
+set @FEN = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R w KQ - 0 12';
+select dbo.FENfreeSquares(@FEN)
