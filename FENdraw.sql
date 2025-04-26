14409fb3e8989fcd7914be2b33502355dac84df7
Alex Hall <alexhall1993@hotmail.co.uk>
2025-04-26 17:19:40 +0100

    Update FENdraw.sql
    now uses a complete fen

--- FENdraw.sql
+++ FENdraw.sql
@@ -1,11 +1,12 @@
-declare @Files as nvarchar(15); 
 declare @FEN as nvarchar(max);
 declare @FENext as nvarchar(max);
 
-set @Files = 'A,B,C,D,E,F,G,H';-- the files 
---set @FEN  = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position 
-set @FEN  = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R'
-set @FENext = @FEN
+--set @FEN  = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position note that the full fen will have additional characters that need to be accounted for
+--set @FEN  = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R'
+set @FEN = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R w KQ - 0 12'
+select @FENext = value from string_split(@fen, ' ', 1) where ordinal = 1 --convert the enpy square into spaces for display on the table later 
+select @FENext
+
 set @FENext = replace(@FENext, 8, space(8));
 set @FENext = replace(@FENext, 7, space(7));
 set @FENext = replace(@FENext, 6, space(6));
@@ -15,9 +16,8 @@ set @FENext = replace(@FENext, 3, space(3));
 set @FENext = replace(@FENext, 2, space(2));
 set @FENext = replace(@FENext, 1, space(1));
 
-select @FEN;
-select @FENext;
 
+-- this bit will convert the FEN notation into a visible table
 with position as 
 (
 	select value as gPosition, Ordinal as grank,
