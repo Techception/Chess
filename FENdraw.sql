e97d4635ec941db7018f594e544a3436504cb736
Alex Hall <alexhall1993@hotmail.co.uk>
2025-04-26 16:52:49 +0100

    Update FENdraw.sql
    imprve fen set

--- FENdraw.sql
+++ FENdraw.sql
@@ -4,8 +4,16 @@ declare @FENext as nvarchar(max);
 
 set @Files = 'A,B,C,D,E,F,G,H';-- the files 
 --set @FEN  = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position 
-set @FEN  = '2R5/5P2/k4P2/4p2p/1p2r3/1K1PB1PP/3PN3/3r4'
-set @FENext = replace(@FEN, 8, space(8));
+set @FEN  = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R'
+set @FENext = @FENext
+set @FENext = replace(@FENext, 8, space(8));
+set @FENext = replace(@FENext, 7, space(7));
+set @FENext = replace(@FENext, 6, space(6));
+set @FENext = replace(@FENext, 5, space(5));
+set @FENext = replace(@FENext, 4, space(4));
+set @FENext = replace(@FENext, 3, space(3));
+set @FENext = replace(@FENext, 2, space(2));
+set @FENext = replace(@FENext, 1, space(1));
 
 select @FEN;
 select @FENext;
