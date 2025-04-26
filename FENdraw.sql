56e443d684a2d50883a81fd167f6b7b817414821
Alex Hall <alexhall1993@hotmail.co.uk>
2025-04-26 17:32:54 +0100

    Update FENdraw.sql
    now its a proc

--- FENdraw.sql
+++ FENdraw.sql
@@ -1,12 +1,18 @@
-declare @FEN as nvarchar(max);
-declare @FENext as nvarchar(max);
-
+create or alter proc FENdraw 
+@FEN as nvarchar(max)
+AS
+-- testing scenarios 
 --set @FEN  = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position note that the full fen will have additional characters that need to be accounted for
 --set @FEN  = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R'
-set @FEN = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R w KQ - 0 12'
-select @FENext = value from string_split(@fen, ' ', 1) where ordinal = 1 --convert the enpy square into spaces for display on the table later 
-select @FENext
+--set @FEN = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R w KQ - 0 12'
 
+BEGIN
+declare @FENext as nvarchar(max);
+set @FENext = @FEN
+select @FENext = value from string_split(@FENext, ' ', 1) where ordinal = 1 -- just get the position part of the FEN
+select @FENext
+--
+--convert the enpy square into spaces for display on the table later 
 set @FENext = replace(@FENext, 8, space(8));
 set @FENext = replace(@FENext, 7, space(7));
 set @FENext = replace(@FENext, 6, space(6));
@@ -32,3 +38,7 @@ with position as
 	from string_split(@FENext, '/', 1)
 ) 
 select * from position
+END
+go
+
+exec FENdraw @FEN = 'r1bq1rk1/pp3ppp/2pp3n/3P4/1nB1P3/2b2N1P/PB3PP1/R2QK2R w KQ - 0 12'
