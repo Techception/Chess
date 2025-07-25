16792e9a61eac0820b23acac81a1ecbc0b1c0253
Hall, Alexander <alexanderhall1993@outlook.com>
2025-07-25 17:08:00 +0100

    made a start on linearised board added excel example of chess board

--- DrawChessboard.sql
+++ DrawChessboard.sql
@@ -1,15 +1,8 @@
+create or alter procedure DrawChessboard
+@FEN nvarchar(max)
+as
+set @FEN = dbo.FENprocessFreeSquares(@FEN);
 
---declare @FEN as nvarchar(max) = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position 
-declare @FEN as nvarchar(max) = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR'; --b KQkq e3 0 1
-
-/*this is a function in FENprocess.sql*/
-while convert(int, substring(@FEN,patindex('%[1-8]%', @FEN),1)) <> 0
-	begin
-		set @FEN = replace(@FEN,convert(int, substring(@FEN,patindex('%[1-8]%', @FEN),1)),space(convert(int, substring(@FEN,patindex('%[1-8]%', @FEN),1))))
-	end;
-	-- this will replace all numbers in the FEN with spaces, so we can split it into rows later
-
--- should change this to a table funtion to make it more reusable
 with position as 
 (
 	select value as gPosition, (9-Ordinal) as grank,
@@ -23,6 +16,11 @@ with position as
 	substring(value, 8,1) as H
 	from string_split(@FEN, '/', 1)
 )
---select *, substring(gPosition, gFileNum, 1) from board as b inner join position as p on b.grank = p.grank order by b.grank, gFileNum
-select * from position
---SELECT * FROM BOARD
+select * from position;
+go
+-----------------------------
+
+--declare @FEN as nvarchar(max) = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';  -- fen staring position 
+declare @FEN as nvarchar(max) = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR'; --b KQkq e3 0 1
+exec DrawChessboard @FEN
+
